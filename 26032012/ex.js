14d693126b8e28c5009eb87dc7caee32a8b8102c
Luigi Cristiano <luigicristiano83@gmail.com>
2012-03-26 16:53:11 +0200

    first ex

---
+++ 26032012/ex.js
@@ -0,0 +1,25 @@
+var d1 = DOMAIN ([[2.5]]) [4])
+var d2 = DOMAIN ([[1,7], [3,5]]) ([4,3])
+var d3 = DOMAIN ([[1,7], [3,5], [2,4]]) ([4,3, 2])
+
+
+
+
+var domain = DOMAIN([[0,1]])([10]);
+
+var mapping = function (p) {
+	var u = p[0];
+	return [u, 1]
+}
+
+
+
+var mapped = MAP(mapping)(domain)
+
+
+
+var bisec = function (p) {
+	var u = p[0];
+	return [u,u]
+
+}
