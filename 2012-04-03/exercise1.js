4defb16e40f0c49398c02da65ca647b8852e9bad
Luigi Cristiano <luigicristiano83@gmail.com>
2012-04-04 14:31:00 +0200

    Homework1 ex1 e ex2

--- 2012-04-03/exercise1.js
+++ 2012-04-03/exercise1.js
@@ -1,3 +1,5 @@
+//Punti per il contorno della pianta
+
 var p1 = [[1,2], [0,2], [0,0]]
 var p2 = [[0,0], [39,0], [39,4]]
 var p3 = [[39,4], [52,4], [52,6]]
@@ -17,65 +19,15 @@ var poly6 = POLYLINE(p6)
 var poly7 = POLYLINE(p7)
 var poly8 = POLYLINE(p8)
 
-var pool = [[1,1], [1,10], [21,10]]
-var pool2 = [[21,10], [21,1], [1,1]]
+
+//Funzioni per disegnare la griglia fatta di riquadri 1x1
 
 
-var polypool = POLYLINE(pool)
-var polypool2 = POLYLINE(pool2)
-
-
-var ext1 = [[41.2, 5], [51, 5], [51, 16]]
-var ext2 = [[51,16], [37.7, 16]]
-
-var extpoly1 = POLYLINE(ext1)
-var extpoly2 = POLYLINE(ext2)
-
-var extPool1 = [[7.8, 1], [1,1], [1,22]]
-var extPool2 = [[1,22], [9,22], [9,17]]
-
-var extPoolpoly1 = POLYLINE(extPool1);
-var extPoolpoly2 = POLYLINE(extPool2);
-
-var extStruct = STRUCT([extpoly1, extpoly2, extPoolpoly1, extPoolpoly2])
-
-var extFrame = COLOR([0.5,0.5,0.5])(extStruct)
-
-
-
-
-var internalPiece1 = [[32, 7.5], [32, 13.7], [31,13.7]]
-var internalPiece2 = [[31,13.7], [31, 7.5]]
-
-var internalPiecePoly1 = POLYLINE(internalPiece1);
-var internalPiecePoly2 = POLYLINE(internalPiece2);
-
-var internalStruct = STRUCT([internalPiecePoly1, internalPiecePoly2])
-var internalFrame = COLOR([0.5,0.5,0.5])(internalStruct);
-
-
-
-
-
-var w1 = [[6.5,15], [26.5, 15]]
-var wall1 = POLYLINE(w1);
-
-var w2 = [[25, 7.5], [34, 7.5]]
-var wall2 = POLYLINE(w2)
-
-var wallStruct = STRUCT ([wall1,wall2])
-
-var wallFrame = COLOR([0.5, 0.5, 0.5])(wallStruct)
-
-
-
-
-
-
-var map = STRUCT([poly1,poly2,poly3, poly4, poly5, poly6, poly7, poly8, polypool, polypool2, extFrame, internalFrame, wallFrame])
-
-
-
+/*Funzione principale per disegnare la griglia.
+IN: p1x: ascissa del punto da cui cominciare a disegnare la griglia
+	p2x: ascissa del punto in cui si finisce di disegnare la griglia
+	fromy: ordinata di partenza da cui disegnare la griglia
+	toy: ordinata di arrivo*/
 
 drawGrid = function(p1x, p2x, fromy, toy) {
 	for (var i = fromy; i<toy+1; i++) {
@@ -84,9 +36,11 @@ drawGrid = function(p1x, p2x, fromy, toy) {
 	var grid = POLYLINE(p)
 
 	DRAW(grid)
-	drawVerticalGrid(fromy, toy, p1x, p2x)
+	drawVerticalGrid(fromy, toy, p1x, p2x) //Funzione richiamata per disegnare l'asse verticale
 }
 }
+
+//Funzione per disegnare le linee verticali della griglia
 
 drawVerticalGrid = function(p1y, p2y, fromx, tox) {
 	for (var i = fromx; i<tox+1; i++) {
@@ -97,8 +51,10 @@ drawVerticalGrid = function(p1y, p2y, fromx, tox) {
 	DRAW(grid)
 }
 }
+
+//Disegno della griglia diviso per zone:
+
 drawGrid(21, 31, 0, 17);
-//drawVerticalGrid(0, 17, 21, 39)
 
 drawGrid(32, 39, 12,17);
 
@@ -115,42 +71,98 @@ drawGrid(32, 39, 12, 17);
 drawGrid(33, 39, 9, 12);
 
 drawGrid(39, 47, 4, 16);
-//drawVerticalGrid(4, 16, 39, 47);
 
 drawGrid(1, 9, 10, 22)
-//drawVerticalGrid(10, 22, 1, 9);
 
 drawGrid(9, 21, 10, 17)
-//drawVerticalGrid(10,17,9,21);
 
 drawGrid(0, 39, 0, 1);
-//drawVerticalGrid(0, 1, 0,39);
 
 drawGrid(47, 52, 4, 5);
 
 
 
+// Contorno del riquadro d'acqua presente sul lato sinistro/frontale della struttura
+
+var pool = [[1,1], [1,10], [21,10]]
+var pool2 = [[21,10], [21,1], [1,1]]
 
 
-DRAW(map)
+var polypool = POLYLINE(pool)
+var polypool2 = POLYLINE(pool2)
 
 
 
 
-/*
-for (var i = 17; i<22; i++) {
-	var p = [[1, i], [9, i]]
 
-	var grid = POLYLINE(p)
+//Contorno del riquadro d'acqua presente sul lato destro/retro della struttura e dei muri 
+//presenti sul lato sinistro e destro, disegnati in nero
 
-	DRAW(grid)
-};
+var ext1 = [[41.2, 5], [51, 5], [51, 16]]
+var ext2 = [[51,16], [37.7, 16]]
+
+var extpoly1 = POLYLINE(ext1)
+var extpoly2 = POLYLINE(ext2)
+
+var extPool1 = [[1,22], [1,1], [7.8, 1]]
+var extPool2 = [[1,22], [9,22], [9,17]]
+
+var extPoolpoly1 = POLYLINE(extPool1);
+var extPoolpoly2 = POLYLINE(extPool2);
+
+var extStruct = STRUCT([extpoly1, extpoly2, extPoolpoly1, extPoolpoly2])
+
+var extFrame = COLOR([0.5,0.5,0.5])(extStruct)
 
 
-for (var i = 10; i > 17; i++) {
-	var p = [[1, i]], [39,i]
+//Muro interno
 
-	var grid = POLYLINE(p)
+var internalPiece1 = [[32, 7.5], [32, 13.7], [31,13.7]]
+var internalPiece2 = [[31,13.7], [31, 7.5]]
 
-	DRAW(grid)
-};*/
+var internalPiecePoly1 = POLYLINE(internalPiece1);
+var internalPiecePoly2 = POLYLINE(internalPiece2);
+
+var internalStruct = STRUCT([internalPiecePoly1, internalPiecePoly2])
+var internalFrame = COLOR([0.5,0.5,0.5])(internalStruct);
+
+
+
+//Muri interni, disegnati in nero
+
+var w1 = [[6.5,15], [26.5, 15]]
+var wall1 = POLYLINE(w1);
+
+var w2 = [[25, 7.5], [34, 7.5]]
+var wall2 = POLYLINE(w2)
+
+var w3 = [[30, 13.7], [40, 13.7]]
+var wall3 = POLYLINE(w3);
+
+var w4 = [[37.2, 11.5], [42.5, 11.5]]
+var wall4 = POLYLINE(w4)
+
+var w5 = [[44.7, 6.9], [44.7, 14.1]]
+var wall5 = POLYLINE(w5)
+
+//Muri parte alta a sinistra
+var w6 = [[5, 20.7], [9, 20.7]]
+var wall6 = POLYLINE(w6)
+
+var w7 = [[7,22], [7,21]]
+var wall7 = POLYLINE(w7)
+
+var wallStruct = STRUCT ([wall1,wall2, wall3, wall4, wall5, wall6, wall7])
+
+var wallFrame = COLOR([0.5, 0.5, 0.5])(wallStruct)
+
+
+
+
+
+
+var plan = STRUCT([poly1,poly2,poly3, poly4, poly5, poly6, poly7, poly8, polypool, polypool2, extFrame, internalFrame, wallFrame])
+
+
+
+DRAW(plan)
---
+++ 2012-04-03/exercise2.js
@@ -0,0 +1,372 @@
+//Basement
+
+var b1 = SIMPLEX_GRID([
+	[1],
+	[2],
+	[1]
+	])
+
+//Parte intorno la vasca frontale
+var b2 = SIMPLEX_GRID([
+	[-1, 20],
+	[1, -9, 7],
+	[1]
+	]);
+
+//Struttura sotto la vasca frontale
+
+var b3 = SIMPLEX_GRID([
+	[-1, 20],
+	[-1, 9],
+	[0.5]
+	])
+
+//Struttura in alto a sinistra
+var b4 = SIMPLEX_GRID([
+	[-1,8],
+	[-17, 5],
+	[1]
+	]);
+
+//Struttura centrale 1
+var b5 = SIMPLEX_GRID([
+	[-21, 15],
+	[17],
+	[1]
+	]);
+
+//Struttura centrale 2
+var b6 = SIMPLEX_GRID([
+	[-36, 3],
+	[-4, 13],
+	[1]
+	])
+
+
+
+//Struttura in basso a destra
+var b7 = SIMPLEX_GRID([
+	[-39, 12],
+	[-4,1],
+	[1]
+	]);
+
+//Angolo in basso a destra
+var b8 = SIMPLEX_GRID([
+	[-51, 1],
+	[-4, 2],
+	[1]
+	]);
+
+//Struttura a destra
+var b9 = SIMPLEX_GRID([
+	[-39, 8],
+	[-4, 12],
+	[1]
+	]);
+
+//Vasca di destra
+var b10 = SIMPLEX_GRID([
+	[-47, 4],
+	[-5, 11],
+	[0.5]
+	])
+
+
+//Struttura sotto i gradini
+var b11 = SIMPLEX_GRID([
+	[-36, 3],
+	[1],
+	[1]
+	]);
+
+//Gradino 1
+var s1 = SIMPLEX_GRID([
+	[-36, 3],
+	[-1, 3],
+	[0.5]
+	]);
+
+//Gradino 2
+var s2 = SIMPLEX_GRID([
+	[-36, 2],
+	[-1, 3],
+	[-0.5, 0.25]
+	]);
+
+//Gradino 3
+var s3 = SIMPLEX_GRID([
+	[-36, 1],
+	[-1, 3],
+	[-0.75, 0.25]
+	])
+
+var steps = STRUCT([s1, s2, s3]);
+
+var basement = STRUCT([b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, steps])
+
+
+//DRAW(basement)
+
+//Muri di sinstra
+
+var wall1 = SIMPLEX_GRID([
+	[-1, 7.5],
+	[-0.8, 0.4],
+	[-1, 3]
+	]);
+
+
+var wall2 = SIMPLEX_GRID([
+	[-1, 0.4],
+	[-1, 9],
+	[-0.5, 3.5]
+	]);
+
+
+var wall3 = SIMPLEX_GRID([
+	[-1, 0.4],
+	[-10, 12],
+	[-1, 3]
+	]);
+
+
+var wall4 = SIMPLEX_GRID([
+	[-1, 8],
+	[-21.6, 0.4],
+	[-1, 3]
+	]);
+
+var wall5 = SIMPLEX_GRID([
+	[-8.6, 0.4],
+	[-17, 5],
+	[-1, 3]
+	]);
+
+
+var sxWalls = STRUCT([wall1, wall2, wall3, wall4, wall5])
+
+
+//Muri di destra
+
+var wall6 = SIMPLEX_GRID([
+	[-37.7, 9.3],
+	[-15.6, 0.4],
+	[-1, 3]
+	]);
+
+var wall7 = SIMPLEX_GRID([
+	[-47, 4],
+	[-15.6, 0.4],
+	[-0.5, 3.5]
+	]);
+
+var wall8 = SIMPLEX_GRID([
+	[-50.6, 0.4],
+	[-5, 11],
+	[-0.5, 3.5]
+	]);
+
+var wall9  = SIMPLEX_GRID([
+	[-47, 4],
+	[-4.6, 0.4],
+	[-0.5, 3.5]
+	]);
+
+var wall10 = SIMPLEX_GRID([
+	[-41.1, 5.9],
+	[-4.6, 0.4],
+	[-1, 3]
+	]);
+
+var dxWalls = STRUCT([wall6, wall7, wall8, wall9, wall10])
+
+
+//Muro fronte piscina
+
+var poolWall = SIMPLEX_GRID([
+	[-6.1, 20.4],
+	[-15.6, 0.4],
+	[-1, 3]
+	]);
+
+
+
+//Mura interne dependance
+var wD1 = SIMPLEX_GRID([
+	[-5, 0.5, -1, 2.5],
+	[-20.6, 0.2],
+	[-1, 3]
+	])
+
+var wD2 = SIMPLEX_GRID([
+	[-6.8, 0.2],
+	[-21.4, 0.6],
+	[-1, 3]
+	])
+
+var wD3 = SIMPLEX_GRID([
+	[-4.8, 0.2],
+	[-17, 1, -1, 3],
+	[-1, 3]
+	])
+
+var wD4 = SIMPLEX_GRID([
+	[-1, 6, -1, 1],
+	[-16.8, 0.2],
+	[-1, 3]
+	])
+
+
+var wallsD = STRUCT([wD1, wD2, wD3, wD4])
+
+
+
+//Mura interne struttura principale
+
+var wallP1 = SIMPLEX_GRID([
+	[-25, 9],
+	[-7.3, 0.2],
+	[-1, 3]
+	]);
+
+var wallP2 = SIMPLEX_GRID([
+	[-30, 10],
+	[-13.6, 0.2],
+	[-1, 3]
+	]);
+
+var wallP3 = SIMPLEX_GRID([
+	[-37.1, 5],
+	[-11.3, 0.2],
+	[-1, 3]
+	])
+	
+
+var centralWall = SIMPLEX_GRID([
+	[-31, 1],
+	[-7.5, 6.1],
+	[-1, 2]
+	])
+
+
+
+var wallsP = STRUCT([wallP1, wallP2, wallP3, centralWall])
+
+
+
+//Creazione intelaiatura muro di vetro
+var grid1 = SIMPLEX_GRID([
+	[0.3, -3.6, 0.3],
+	[0.2],
+	[3]
+	]);
+
+var grid2 = SIMPLEX_GRID([
+	[-0.3, 3.6],
+	[0.1],
+	[0.1, -2.8, 0.1]
+	]);
+
+var wind = STRUCT([grid1, grid2])
+
+
+//Creazione muro di vetro 1
+
+var t1 = T([0])([4])
+
+var glassWall1Struct = STRUCT([wind,t1, wind, t1, wind])
+
+
+var glassWall1 = T([0,1,2])([29, 4.6, 1])(glassWall1Struct)
+
+
+//Creazione intelaiatura secondo muro vetro
+
+var gridB1 = SIMPLEX_GRID([
+	[0.1],
+	[0.1, -0.8, 0.1],
+	[3]
+	]);
+
+var gridB2 = SIMPLEX_GRID([
+	[0.1],
+	[1],
+	[0.1, -2.8, 0.1]
+	]);
+
+
+var wind2 = STRUCT([gridB1, gridB2])
+
+//Creazione muro di vetro 2
+
+var t2 = T([1])([1])
+
+var glassWall2Struct = STRUCT([wind2, t2, wind2, t2, wind2, t2, wind2, t2, wind2, t2, wind2, t2, wind2, t2, wind2])
+
+
+var glassWall2 = T([0,1,2])([44.2, 6, 1])(glassWall2Struct)
+
+var walls = STRUCT([sxWalls, dxWalls, poolWall, wallsD, wallsP, glassWall1, glassWall2])
+
+
+
+//DRAW(walls)
+
+
+//Creazione tetto dependance
+
+var roofD = SIMPLEX_GRID([
+	[10],
+	[-13, 9],
+	[-4, 1]
+	])
+
+
+//Creazione tetto edificio principale
+
+var roofP = SIMPLEX_GRID([
+	[-24, 23],
+	[-4, 13],
+	[-4, 1]
+	])
+
+var roofs = STRUCT([roofD, roofP])
+
+
+//DRAW(roofs)
+
+//Creazione panca
+
+//Sostegni panca
+
+var c = SIMPLEX_GRID([
+	[0.5],
+	[0.5],
+	[0.3]
+	])
+
+var t3 = T([0])([1.5])
+
+var cseries = STRUCT([c, t3, c, t3, c, t3, c, t3, c, t3, c, t3, c, t3, c, t3, c])
+
+var sostegni = T([0,1,2])([7.3, 14.2, 1])(cseries)
+
+
+//panca
+
+var lastraPanca = SIMPLEX_GRID([
+	[-6.5, 14],
+	[-14, 0.9],
+	[-1.3, 0.4]
+	])
+
+var panca = STRUCT([sostegni, lastraPanca])
+
+
+
+
+var pavilion = STRUCT([basement, walls, roofs, panca])
+
+
+DRAW(pavilion)
