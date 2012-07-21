1ab6dcf3bdf467f689eb4b800fe41d7fcd6a2160
Luigi Cristiano <luigicristiano83@gmail.com>
2012-07-21 13:09:16 +0200

    code update

--- final-project/code.js
+++ final-project/code.js
@@ -1,4 +1,4 @@
-//VELA
+//Sail
 
 var turns = 1.5;
 var angle = turns * 2 * PI;
@@ -12,20 +12,20 @@ var mapping = function (p) {
   var h = height - (a / angle * height);
   return [r * -COS(a), r * SIN(a), h];
 };
-var vela = MAP(mapping)(domain2);
+var sail = MAP(mapping)(domain2);
 
-var posVela = T([2])([22])(vela)
+var possail = T([2])([22])(sail)
 
-var cVela = COLOR([1,1,1])(posVela);
+var csail = COLOR([1,1,1])(possail);
 
 
 
-//Base centrale
+//Central base
 
 var b = CYL_SURFACE([16,1])([100,1])
-var angolo = 2 * PI;
-var raggio = 16;
-var domain3 = DOMAIN([[0, angolo], [0, raggio]])([120, 4])
+var angle = 2 * PI;
+var radius = 16;
+var domain3 = DOMAIN([[0, angle], [0, radius]])([120, 4])
 
 var mapping3 = function (p) {
   var a = p[0];
@@ -39,11 +39,11 @@ var dPos = T([2])([1])(d)
 var base1 = STRUCT([b,dPos]) 
 
 
-//Base esterna
+//External base
 
 var b2 = CYL_SURFACE([20,0.5])([100,1])
-var raggio = 20;
-var domain3 = DOMAIN([[0, angolo], [0, raggio]])([120, 4])
+var radius = 20;
+var domain3 = DOMAIN([[0, angle], [0, radius]])([120, 4])
 
 var mapping3 = function (p) {
   var a = p[0];
@@ -58,22 +58,24 @@ var base2 = STRUCT([b2,d3,d4])
 
 
 
-//box interno
+//Internal box
+
 var box = T([0,1,2])([-2.5,-2.5,1])(CUBOID([5,5,5]))
 DRAW(COLOR([230/255,190/255,135/255])(box))
 
 
-//Pilone
+//The mast
 
 var p = CYL_SURFACE([1,32])([100,1])
-var posPil = T([2])([6])(p)
+var posMast = T([2])([6])(p)
 
 
-//Punta pilone
-var angolo = 2 * PI;
-var raggio = 0.5; 
+//Mast's tip
+
+var angle = 2 * PI;
+var radius = 0.5; 
 var altezza = 2; 
-var domain3 = DOMAIN([[0, angolo], [0, raggio], [0, altezza]])([30, 4, 4]); 
+var domain3 = DOMAIN([[0, angle], [0, radius], [0, altezza]])([30, 4, 4]); 
 var mapping3 = function (p) { 
   var a = p[0];  
   var r = p[1];  
@@ -84,61 +86,63 @@ var tip = MAP(mapping3)(domain3);
 
 var posTip = T([2])([38])(tip)
  
-var pilone = STRUCT([posPil, posTip])
+var mast = STRUCT([posMast, posTip])
 
 
 
 
 
-//Aste
+//Spars
 
-var supAsta = T([2])([1])(CYL_SURFACE([0.5, 6])([100,1]))
+var surfSp = T([2])([1])(CYL_SURFACE([0.5, 6])([100,1]))
 
-//Circonferenza per il tappo, non è possibile usare DISK per il bug dell'alpha di PLASM.js
-var angolo = 2 * PI;
-var raggio = 0.5;
-var domain3 = DOMAIN([[0, angolo], [0, raggio]])([120, 4])
+//The circumference for the cap
+
+var angle = 2 * PI;
+var radius = 0.5;
+var domain3 = DOMAIN([[0, angle], [0, radius]])([120, 4])
 
 var mapping3 = function (p) {
   var a = p[0];
   var r = p[1];
   return [r * COS(a), r * SIN(a),0];
 };
-var tappo = MAP(mapping3)(domain3);
-var posTappo = T([2])([7])(tappo)
 
-var rotAsta = R([1,2])([PI/2])
+var cap = MAP(mapping3)(domain3);
+var posCap = T([2])([7])(cap)
 
-var asta = STRUCT([rotAsta, posTappo, supAsta])
+var rotSpar = R([1,2])([PI/2])
 
-
-
-
-
-var rotAste = R([0,1])([PI/2])
-
-var aste = T([2])([7])(STRUCT([asta, rotAste, asta, rotAste, asta, rotAste, asta]))
+var spar = STRUCT([rotSpar, posCap, surfSp])
 
 
 
 
 
+var rotSpars = R([0,1])([PI/2])
 
-//Sostegni vela
-
-
-var sost = SCALE([0,1,2])([0.5,2.52,0.5])(asta)
-
-var posSost1 = T([0,2])([1.5, 37])(R([0,1])([-PI/2])(sost))
-
-var sost2 = SCALE([0,1,2])([2.05,1.1,1.1])(posSost1)
-
-var posSost2 = T([0,2])([-1.2,-18.7])(R([0,1])([PI])(sost2))
-
-var sostegni = STRUCT([posSost1, posSost2])
+var spars = T([2])([7])(STRUCT([spar, rotSpars, spar, rotSpars, spar, rotSpars, spar]))
 
 
-//Travi di supporto per la base esterna
+
+
+
+
+//Sail's spars
+
+
+var sailSp = SCALE([0,1,2])([0.5,2.52,0.5])(spar)
+
+var posSailSp = T([0,2])([1.5, 37])(R([0,1])([-PI/2])(sailSp))
+
+var sailSp2 = SCALE([0,1,2])([2.05,1.1,1.1])(posSailSp)
+
+var posSailSp2 = T([0,2])([-1.2,-18.7])(R([0,1])([PI])(sailSp2))
+
+var Sspars = STRUCT([posSailSp, posSailSp2])
+
+
+//Beams for the external base
 
 
 var domain = INTERVALS(1)(50);
@@ -156,8 +160,8 @@ var sezMcp2 = [[0,0,0.5],[6,7,0.5],[6,7,0.5],[6,7,0.5],[6,7,0.5],[6,7,0.5],[6,7,
 var sezMc2 = BEZIER(S0)(sezMcp2);
 
 //Surface 1
-var sup = BEZIER(S1)([sezMc1,sezMc2])
-var rSup = MAP(sup)(domain2)
+var sur = BEZIER(S1)([sezMc1,sezMc2])
+var rSur = MAP(sur)(domain2)
 
 
 
@@ -173,38 +177,39 @@ var bordC2 = BEZIER(S0)(bordP2);
 
 
 //Surface 2
-var sup2 = BEZIER(S1)([bordC1,bordC2])
-var rSup2 = MAP(sup2)(domain2)
+var sur2 = BEZIER(S1)([bordC1,bordC2])
+var rSur2 = MAP(sur2)(domain2)
 
 
-//Disegno struttura di travi
+//Beams structure
 
-var support = STRUCT([rSup,rSup2])
+var support = STRUCT([rSur,rSur2])
 
 var supportPos = T([0,1,2])([-19,0.2,0.5])(S([0,2])([3,3])(R([1,2])([PI/2])(support)))
 
 
-var supportStruct = STRUCT([supportPos, rotAste, supportPos, rotAste, supportPos, rotAste, supportPos])
+var supportStruct = STRUCT([supportPos, rotSpars, supportPos, rotSpars, supportPos, rotSpars, supportPos])
 
 
 
 
 
-//Modulo di aggancio per le travi
-var modTr = CYL_SURFACE([2.5,1.5])([100,1])
-var posMod = T([2])([20])(modTr)
+//Contact module for the beams
 
-var tapMod = T([2])([20])(DISK(2.5)([100,1]))
-var tapMod2 = T([2])([21.5])(DISK(2.5)([100,1]))
+var modBeam = CYL_SURFACE([2.5,1.5])([100,1])
+var posMod = T([2])([20])(modBeam)
+
+var capMod = T([2])([20])(DISK(2.5)([100,1]))
+var capMod2 = T([2])([21.5])(DISK(2.5)([100,1]))
 
 
 DRAW(COLOR([230/255,190/255,135/255])(posMod))
-DRAW(tapMod)
-DRAW(tapMod2)
+DRAW(capMod)
+DRAW(capMod2)
 
 
 
-//Tiranti
+//Tie rods
 
 var tir1 = POLYLINE([[8.8,18,0],[11,13,33.4]])
 var tir2 = POLYLINE([[17.9,8,0],[19,7,32.5]])
@@ -220,25 +225,25 @@ var tirs = STRUCT([tir1,tir2,tir3,tir4,tir5,tir6,tir7,tir8])
 var cTirs = COLOR([0.5,0.5,0.5])(tirs)
 
 
-var wooden = STRUCT([base1,base2,pilone,aste,sostegni,supportStruct])
+var wooden = STRUCT([base1,base2,mast,spars,Sspars,supportStruct])
 var woodenFrame = COLOR([237/255,180/255,100/255])(wooden)
 
 
-var vite = STRUCT([cVela,woodenFrame,cTirs])
+var vite = STRUCT([csail,woodenFrame,cTirs])
 
 DRAW(vite)
 
 
 
-//Funzione che implementa l'animazione della rotazione della Vite con tasto di conferma per attivarla
+//Function that implements the animation for the Vite Aerea with confirm button
 
 
 function Fly() {
-  var messaggio = 'Vuoi attivare il movimento della vite aerea?'
+  var message = 'Do you want to activate the movement of the Vite Aerea?'
 
-  var scelta = confirm(messaggio)
+  var choice = confirm(message)
 
-  if (scelta == true) {
+  if (choice == true) {
     setInterval(function () {
     vite.rotate([0,1], PI/45);
     }, 60);
